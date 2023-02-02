6b3eef202c1712092d7c2951597348be14764591
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-02 14:20:39 -0800

    --add rn-elements

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,19 +1,47 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from '@rneui/themed'
 
 // Components
 import { CustomersScreen } from '../screens/CustomersScreen';
 import { OrdersScreen } from '../screens/OrdersScreen';
 
+export type TabStackParamList = {
+  Customers: undefined;
+  Orders: undefined;
+}
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
 export function TabNavigator() {
 
-  const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="CustomersScreen" component={CustomersScreen}/>
-      <Tab.Screen name="OrdersScreen" component={OrdersScreen}/>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "lightslategray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Customers') {
+            return (
+              <Icon 
+                name="users"
+                type='feather'
+              />
+            )
+          } else if (route.name === 'Orders') {
+            return (
+              <Icon 
+                name='package'
+                type='feather'
+              />
+            )
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Customers" component={CustomersScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
   )
 }
