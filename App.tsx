e392ee8545454122599b09c1266bef4229cdb33c
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-02 23:41:14 -0800

    --add graphql types

--- App.tsx
+++ App.tsx
@@ -6,13 +6,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // Components
 import { CustomersScreen } from './screens/CustomersScreen';
 import { RootNavigator } from './navigator/RootNavigator';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://mornington.stepzen.net/api/idle-toucan/__graphql',
+  headers: {'Authorization':'apikey mornington::stepzen.io+1000::03bd1086ef287cee8f5e2a20eb5f776650ea378076acc7e002e706ef160cfe31'},
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
